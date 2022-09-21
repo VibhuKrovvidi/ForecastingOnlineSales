1304194574ae454f367d1afb7eff1768babcd6e5
vibhukrovvidi <vibkrovvidi@gmail.com>
2022-09-21 18:18:44 -0400

    Rearrange files to de-clutter codebase

--- data.py
+++ data.py
@@ -3,11 +3,11 @@ from datetime import datetime
 from dateutil import parser
 
 # convert the time stamps to datetime 
-df = pd.read_csv("online_retail_uk_data.csv")
+df = pd.read_csv("./Datasets/online_retail_uk_data.csv")
 df['InvoiceDate'] = df['InvoiceDate'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S"))
 
 # read in unemployment data and process
-unemployment = pd.read_csv('unemployment.csv')
+unemployment = pd.read_csv('./Datasets/unemployment.csv')
 unemployment['Month'] = unemployment['Month'].apply(lambda x: datetime.strptime(x, '%Y %b'))
 
 # merge unemployment and dataset by month
@@ -16,7 +16,7 @@ df = pd.merge(df,unemployment, left_on=df['InvoiceDate'].apply(lambda x: (x.year
                 how='left').iloc[:,1:].drop(columns=['Month'], axis=1)
 
 # merge retail spending by month
-retail_spending = pd.read_excel('retail_spending.xlsx',sheet_name='ValNSAT', header=4)
+retail_spending = pd.read_excel('./Datasets/retail_spending.xlsx',sheet_name='ValNSAT', header=4)
 retail_spending = retail_spending.drop([0,1])[['Time Period','All Retailing excluding automotive fuel']]
 retail_spending['Time Period'] = retail_spending['Time Period'].apply(lambda x: parser.parse(x))
 df = pd.merge(df,retail_spending, left_on=df['InvoiceDate'].apply(lambda x: (x.year, x.month)),
@@ -26,7 +26,7 @@ df = df.rename(columns = {'All Retailing excluding automotive fuel': 'Retail Spe
 
 
 # merge earnings data
-earnings = pd.read_csv("earning.csv")
+earnings = pd.read_csv("./Datasets/earning.csv")
 earnings.rename(columns={ "Time": "Month" }, inplace = True)
 earnings["Month"] = earnings['Month'].apply(lambda x: datetime.strptime(x, "%b-%y"))
 
@@ -38,7 +38,7 @@ df = pd.merge(df,earnings, left_on=df['InvoiceDate'].apply(lambda x: (x.year, x.
 
 # df.to_excel('debug.xlsx')
 
-ftse = pd.read_csv("FTSE 100 Historical Data.csv")
+ftse = pd.read_csv("./Datasets/FTSE 100 Historical Data.csv")
 ftse = ftse[['Date', 'Price']]
 ftse["Date"] = ftse['Date'].apply(lambda x: parser.parse(x))
 print(ftse)
@@ -49,4 +49,4 @@ df = pd.merge(df, ftse, left_on = df['InvoiceDate'].apply(lambda x: (x.year, x.m
 
 print(df)
 
-df.to_csv('merged_dataset.csv')
+df.to_csv('./Datasets/merged_dataset.csv')
